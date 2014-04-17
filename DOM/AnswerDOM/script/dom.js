bcb597630be60072f870b8d46dfd806ac5cdcbeb
koji.kita <koji.kita@mixi.co.jp>
2014-04-17 20:08:16 +0900

    add dom answer

--- DOM/AnswerDOM/script/dom.js
+++ DOM/AnswerDOM/script/dom.js
@@ -9,14 +9,21 @@ document.getElementById("second").style.fontSize = "30px";
 // 3行目のテキストを前から3文字だけにする
 document.getElementById("list").childNodes[5].innerText = document.getElementById("list").childNodes[5].innerText.substring(0,3);
 
+document.getElementById("list").children[2].innerText = document.getElementById("list").children[2].innerText.substring(0,3);
+
 
 // jQuery
 
 // 1行目のテキストを赤色にする
+$("#first").css("color", "red");
 
 // 4行目を追加する
+$("#list").append("<li>4行目のテキストです。</lib>");
 
 // テキストをクリックしたら、何行目かを表示させる（表示方法は自由）
+$("li").on("click", function() {
+    alert($(this).text().slice(0,3));
+});
 
 
 // last year
@@ -25,7 +32,12 @@ document.getElementById("list").childNodes[5].innerText = document.getElementByI
 $j("#myArea .profilePhoto .name span:first a").text("JavaScript").css("font-size", "50px");
 
 // 上に並んでいるメニューボタン（プロフィール、友人リスト、つぶやき・・・）のうちどれか1つを非表示にする
+$j("div.personalNavigation").find("li.myMixi").hide();
 
 // 左右のカラムを半透明にする（1行で）
+$j("div#bodySide02, div#bodySub02").css("opacity", 0.5);
 
 // フィードに表示されている友人全員のニックネームの末尾に、++をつける
+_.each($j("div.feedContent p.name"), function(name) {
+    $j(name).find("a").text($j(name).find("a").text() + "++");
+});
